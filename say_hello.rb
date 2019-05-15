8b1cc7be2dce4d1da229d652f5da01eb6d73998b
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-05-15 02:13:08 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,4 @@
 # Build your say_hello method here
-def introduction (name)
-    puts "Hi, my name is #{name}."
+def say_hello (name = "Ruby Programmer")
+  puts "Hello #{name}!"
 end
-introduction ("Josh")
-
-def introduction_with_language (name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
-end
-introduction_with_language ("Dan", "Ember.js")
